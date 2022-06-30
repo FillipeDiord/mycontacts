b602c546ff32553b18c51cc205448d7288e87fc2
Fillipe Diord <fillipe.diord@hotmail.com>
2022-06-30 08:56:23 -0300

    Created add contact route

--- src/routes.js
+++ src/routes.js
@@ -9,4 +9,6 @@ router.get('/contacts/:id', ContactController.show);
 
 router.delete('/contacts/:id', ContactController.delete);
 
+router.post('/contacts', ContactController.store);
+
 module.exports = router;
